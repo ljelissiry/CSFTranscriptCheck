66a8896b06edcadf0d8f4e81b34066ce277d6f7a
NolanBonnie <nrbmee@gmail.com>
2017-01-15 15:37:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-import selenium
+import time
+import string
+import random
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+nlocation = "/Users/nolanbonnie/Desktop/Python/chromedriver"
+elocation =
+llocation = 
+
+
+
+driver = webdriver.Chrome(location)
+
+driver.get("https://docs.google.com/spreadsheets/d/1ch5pT5ywKXvINhlhDJzvdl0YgxRjSdHKPzIf-ht5qgw/edit#gid=1082589385")
 
