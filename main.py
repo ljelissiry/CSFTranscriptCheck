668d480cd2bf3b05223d55923e1a96534be1a697
NolanBonnie <nrbmee@gmail.com>
2017-01-15 17:25:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
 nlocation = "/Users/nolanbonnie/Desktop/Python/chromedriver"
-elocation =
-llocation = 
+elocation = ""
+llocation = ""
 
 loc = input("Who is using the code?")
 if loc == "nolan":
@@ -18,37 +18,58 @@ elif loc == "luke":
     driver = webdriver.Chrome(llocation)
 else:
     print("input not recognized")
-
+    
 driver.get("https://docs.google.com/spreadsheets/d/1ch5pT5ywKXvINhlhDJzvdl0YgxRjSdHKPzIf-ht5qgw/edit#gid=1082589385")
+
+#Input Email--------------------------------------------------
+elem = driver.find_element_by_name('Email')
+elem.send_keys('nrbmee@gmail.com')
+elem.send_keys(Keys.RETURN)
+#Input Password--------------------------------------------------
+time.sleep(.5)
+elem = driver.find_element_by_name('Passwd')
+elem.send_keys("chase5135")
+elem.send_keys(Keys.RETURN)
+
 startrow = int(input("What row do you want to start on?"))
 endrow = int(input("What row do you want to end on?"))
 rnge = endrow - startrow
 
 #String text = driver.findElement(By.id("some id")).getText()
 
+#Comparing Google Sheets Input with Transcript
 for i in range(rnge):
     scroll = startrow - 1 + i
     for v in range(scroll):
         actions = ActionChains(driver)
         actions.send_keys(Keys.ARROW_DOWN)
         actions.perform()
+    for v in range(4):
+        actions = ActionChains(driver)
+        actions.send_keys(Keys.ARROW_RIGHT)
+        actions.perform()
+    elem = driver.find_element_by_class_name('cell-input')
+    SheetsLastName = elem.get_attribute('innerText')
+    actions = ActionChains(driver)
+    actions.send_keys(Keys.ARROW_RIGHT)
+    actions.perform()
+    elem = driver.find_element_by_class_name('cell-input')
+    SheetsID = int(elem.get_attribute('innerText'))
+    actions = ActionChains(driver)
+    actions.send_keys(Keys.ARROW_RIGHT)
+    actions.perform()
+    elem = driver.find_element_by_class_name('cell-input')
+    SheetsGrade = int(elem.get_attribute('innerText'))
     for v in range(5):
         actions = ActionChains(driver)
         actions.send_keys(Keys.ARROW_RIGHT)
         actions.perform()
-    actions = ActionChains(driver)
-    actions.key_down(Keys.COMMAND)
-    actions.send_keys("C")
-    actions.key_up(Keys.COMMAND)
-    actions.perform()
-    
-        
+    elem = driver.find_element_by_class_name('cell-input')
+    transcriptlink = elem.get_attribute('innerText')
+    driver.get(transcriptlink)
+
     transcript = "NO"
     reason = ""
-
-    SheetsLastName = "Column E"
-    SheetsID = int("Column F")
-    SheetsGrade = int("Column G")
 
     TranscriptLastName = "Michaud"
 
@@ -66,3 +87,4 @@ for i in range(rnge):
             reason = "ID does not match"
     else:
         reason = "Name does not match"
+
